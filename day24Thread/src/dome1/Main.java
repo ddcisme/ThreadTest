7a918de1e98b408c0f880747bc8da077c35d0e23
ddcisme <dengdaoci@126.com>
2017-03-09 09:31:48 +0800

    试试git

--- day24Thread/src/dome1/Main.java
+++ day24Thread/src/dome1/Main.java
@@ -18,6 +18,7 @@ public class Main {
         SellStatic sell=new SellStatic();
         //开始
         Thread thread1 =new Thread(sell,"黄日天");
+        System.out.println(thread1);
         Thread thread2 =new Thread(sell,"赵怼地");
         thread1.start();
         thread2.start();
